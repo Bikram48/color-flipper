9bb0454f5d84dc35a360a474688d0d7eb1f9d50a
Bikram48 <chandbikram001@gmail.com>
2022-03-29 08:12:51 +0545

    use - array instead of random hex color

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let color_generator_btn = document.querySelector( '.click-btn' );
 color_generator_btn.addEventListener( 'click', generate_color );
 
+const colors = {
+    color: [ "red", "green", "#4287f5","#42f5b6","#d4f542" ]
+}
+
 function generate_color() {
-    let color = Math.floor( Math.random() * 16777215 ).toString( 16 );
+    let randomNumber = Math.floor( Math.random() * colors.color.length );
+    let color = colors.color[ randomNumber ];
     display_color( color );
     apply_background_color( color );
 }
 
 function display_color( color ) {
     let display_color_btn = document.querySelector( '.btn-display' );
-    display_color_btn.innerHTML = "Background Color : #" + color;
+    display_color_btn.innerHTML = "Background Color : " + color;
 }
 
 function apply_background_color( color ) {
     let element = document.querySelector( '.container' );
-    element.style.backgroundColor = '#' + color;
+    element.style.backgroundColor = color;
 }
